0698ad328ced4afa5d7dadc01613ede7b347fbb8
Andrey Baznikin <a.baznikin@gmail.com>
2021-01-18 21:31:15 +0700

    add_torrent.sh example - clean output

--- add_torrent.sh
+++ add_torrent.sh
@@ -6,7 +6,8 @@ if [ "$#" -ne 3 ]; then
 fi
 
 ID=$1
-DAYS=$2
+DAYS=$2	# if your torrent client allow you to specifiy priority - you can use it
+
 TORRENT_PATH=$(realpath "$3" 2>/dev/null)			# /mnt/tank/share/music/RUTracker-Keepers/2287-jazz/.torrents/1234688.torrent
 TARGET_PATH=$(realpath `dirname $TORRENT_PATH`/..)		# /mnt/tank/share/music/RUTracker-Keepers/2287-jazz
 INCOMPLETE_PATH="$(realpath $TARGET_PATH)-incomplete"	# /mnt/tank/share/music/RUTracker-Keepers/2287-jazz-incomplete
@@ -21,4 +22,10 @@ mkdir -p "${INCOMPLETE_PATH}/${ID}"
 DL_TARGET=$(echo $TARGET_PATH | sed -E "s/\/mnt\/tank\/share\/music\/RUTracker-Keepers/\/Shared\/RUTracker-Keepers/")	# /Shared/RUTracker-Keepers/2287-jazz
 DL_INCOMPLETE=$(echo $INCOMPLETE_PATH | sed -E "s/\/mnt\/tank\/share\/music\/RUTracker-Keepers/\/Shared\/RUTracker-Keepers/")
 DL_TORRENT=$(echo $TORRENT_PATH | sed -E "s/\/mnt\/tank\/share\/music\/RUTracker-Keepers/\/Shared\/RUTracker-Keepers/")
-echo `iocage exec deluge "LANG=ru_RU.UTF-8 deluge-console -c /usr/local/etc/deluge/ 'add -m ${DL_TARGET}/${ID}/ -p ${DL_INCOMPLETE}/${ID}/ ${DL_TORRENT}'"`;
+OUT=`iocage exec deluge "LANG=ru_RU.UTF-8 deluge-console -c /usr/local/etc/deluge/ 'add -m ${DL_TARGET}/${ID}/ -p ${DL_INCOMPLETE}/${ID}/ ${DL_TORRENT}'"`;
+
+if [ "$(echo "$OUT" | grep "Torrent added")" != "" ]; then
+ echo "OK"
+else
+  echo "FAILED: $OUT"
+fi
